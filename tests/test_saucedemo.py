d33c8b3eebcf027f2e4098cafc3b2755d25a118e
Mahek Verma <147248342+MahekVerma04@users.noreply.github.com>
2026-07-16 10:18:19 +0530

    Add files via upload

---
+++ tests/test_saucedemo.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import Page, expect
+import pytest
+
+
+def test_sauce_demo_sd_1(page: Page):
+    page.goto('https://saucedemo.com')
+
+    page.get_by_placeholder('Username').fill('standard_user')    
+    page.get_by_placeholder('Password').fill('secret_sauce')    
+    page.get_by_role('button',name='login').click()
+    #expect(page).to_have_url('https://saucedemo.com/inventory.html')
+    expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
+    page.get_by_role('button', name='Add to cart').first.click()
+
+def login(page: Page, user, password):
+    page.goto('https://saucedemo.com')
+    page.get_by_placeholder('Username').fill(user)
+    page.get_by_placeholder('Password').fill(password)
+    page.get_by_role('button', name='Login').first.click()
+
+
+@pytest.mark.parametrize(
+        'user, password',
+        [
+            ('standard_user','secret_sauce'),
+            ('visual_user','secret_sauce')
+        ]
+)
+
+
+def test_login_sd_1(page,user, password):
+    login(page,user,password)
+    assert 'inventory' in page.url
+
---
+++ upload_to_xray.py
@@ -0,0 +1,42 @@
+import os
+import requests
+
+# ⚠️ Configure these values!
+CLIENT_ID = os.getenv("XRAY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("XRAY_CLIENT_SECRET")
+ # From Jira > Apps > Xray > API Keys
+PROJECT_KEY = "SD" 
+
+print("1. Authenticating with Xray Cloud...")
+auth_url = "https://xray.cloud.getxray.app/api/v2/authenticate"
+auth_resp = requests.post(
+    auth_url, 
+    json={"client_id": CLIENT_ID, "client_secret": CLIENT_SECRET}
+)
+
+if auth_resp.status_code != 200:
+    print(f"Authentication failed: {auth_resp.text}")
+    exit(1)
+
+token = auth_resp.text.replace('"', '') # Strip quotes off the token
+print("Authentication successful!")
+
+print("\n2. Uploading JUnit results file to Xray...")
+# Endpoint for importing standard JUnit XML
+import_url = f"https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey={PROJECT_KEY}"
+
+# Update headers to indicate we are sending raw XML
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "text/xml"  # <-- Crucial change!
+}
+with open("results/junit-report.xml", "r", encoding="utf-8") as f:
+    xml_data = f.read()
+    response = requests.post(import_url, headers=headers, data=xml_data) # <-- Sending raw XML
+
+if response.status_code == 200:
+    print("Success! Results imported.")
+    print(f"Jira Response: {response.text}")
+else:
+    print(f"Upload failed: {response.status_code}")
+    print(response.text)
